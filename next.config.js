d2ddb5708773003983d581b8ac3673365e7a2cc0
kayes360 <imrulkayes560@gmail.com>
2024-02-26 23:09:45 +0600

    first commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+const withPWA = require('next-pwa')({
+    dest: 'public'
+})
+
+ module.exports = withPWA({
+
+ })
+ module.exports = withPWA({
+    
+ })
+export default nextConfig;
