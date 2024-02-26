25695ca8d27b3e8cbdfa2a825076cb303bb419ba
kayes360 <imrulkayes560@gmail.com>
2024-02-26 23:25:41 +0600

    pwa-init

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
 
 const withPWA = require('next-pwa')({
     dest: 'public'
 })
 
- module.exports = withPWA({
-
- })
- module.exports = withPWA({
-    
- })
-export default nextConfig;
+const nextConfig = {};
+module.exports = withPWA(nextConfig)
